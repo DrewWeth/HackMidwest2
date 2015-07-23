b80d9d4634193ae82ecd2800e5c42053e5a16607
DrewWeth <dgwetherington@gmail.com>
2015-07-23 03:36:54 -0500

    api updates

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,18 +2,20 @@ class ApiController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def post_update_progress
-    youtube_link = params["youtube_link"].to_s
+    youtube_link = params["youtube"].to_s
     progress = params["progress"].to_s
 
     if not youtube_link.blank? or not progress.blank?
-      result = Link.where(:youtube_link =>youtube_link).take
-      result.progress = progress
-      if progress.downcase == "distributed".downcase
-        result.seen_count += 1
+      if result = Link.where(:full_link =>youtube_link).take
+        result.progress = progress
+        if progress.downcase == "distributed".downcase
+          result.seen_count += 1
+        end
+        result.save
+        render :json => result
+      else
+        render :json => result
       end
-      result.save
-
-      render :json => result
 
     else
       render :json => "FAILED. Missing youtube_link or progress. " + params.to_s
@@ -22,7 +24,6 @@ class ApiController < ApplicationController
 
   def create
     @link = Link.new
-
     # Custom
     @link.full_link = params["full_link"]
     # Setting youtube_link
@@ -35,31 +36,14 @@ class ApiController < ApplicationController
     else
       render :json => "FAILED. No full_link or link not unique"
     end
-
-  end
-
-  def elastic_create
-    anti_bot = params["pass"]
-	if anti_bot.eql? "noentry" 
-		if body = JSON.parse(request.body.read)
-			render :json => post_json("http://localhost:9200/ghost/videos", body) 
-		else
-			render :json => "Cannot parse request.body.read"	
-		end
-	else
-		render :json => "Password incorrect."
-	end	
-
   end
 
   def channel_create
     args = {}
     args["url"] = params["url"]
-
     channel = Channel.new(args)
 
     if args["url"]
-
       if channel.save
         render :json => channel
       else
@@ -98,13 +82,4 @@ class ApiController < ApplicationController
     render :json => new_video
   end
 
-	private
-	def post_json(url, content)
-		uri = URI(url)
-		http = Net::HTTP.new(uri.host, uri.port)
-		req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-		req.body = content.to_json
-		res = http.request (req)
-		return res.body
-	end
 end
